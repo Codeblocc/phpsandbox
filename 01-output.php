4b6a1cd3b70ec890acf6f5367bf9e6c677c099e0
Abhishek Dhar <68233166+Codeblocc@users.noreply.github.com>
2022-05-13 22:32:27 +0530

    Add files via upload

---
+++ 01-output.php
@@ -0,0 +1,18 @@
+<?php
+// echo - Output strings, numbers, html, etc
+// echo 123, 'hello', 10.5;
+
+// print - works like echo, but can only take in a single argument
+// print 123;
+
+// print_r() - Print single values and arrays
+// print_r([1,2,3]);
+
+// var_dump() - Returns more info like data types and length
+// var_dump('hellooooo');
+// var_dump(true);
+
+// var_export() - Similar to var_dump(), Outputs a string representation of a variable
+// var_export('hello');
+
+?>
