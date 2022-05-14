3d4b7364c5ecd13f3e9675e058e806cc676ffe86
Abhishek Dhar <68233166+Codeblocc@users.noreply.github.com>
2022-05-14 16:35:07 +0530

    Add files via upload

---
+++ data-types.php
@@ -0,0 +1,29 @@
+<?php
+define('PI', 3.14); // defining a constant
+
+// Data types in PHP
+// 1. String
+// 2. Integer
+// 3. Float
+// 4. Boolean
+// 5. Array
+// 6. Object
+
+echo "<h1> Data Types </h1>";
+
+$var = "this is a string";
+echo var_dump($var);
+echo "<br>";
+
+$var = 5;
+echo var_dump($var);
+echo "<br>";
+
+$var = 5.9;
+echo var_dump($var);
+echo "<br>";
+
+$var = true;
+echo var_dump($var);
+echo "<br>";
+echo PI;
