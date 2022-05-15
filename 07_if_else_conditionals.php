756a4bb3240bd38216cae478e50e6280afc83fcf
Abhishek Dhar <68233166+Codeblocc@users.noreply.github.com>
2022-05-15 22:39:50 +0530

    Add files via upload

---
+++ 07_if_else_conditionals.php
@@ -0,0 +1,32 @@
+<?php
+
+// $a = 665;
+// $b = 9;
+
+// if($a > 78){
+//     echo "a is greater than 78";
+// }
+// else{
+//     echo "a is not greater than 78";
+// }
+
+$age = 54;
+
+// If-Else ladder
+
+if ($age>18){
+    echo "you can drive";
+}
+elseif($age>13){
+    echo "you can sit in the passenger seat";
+}
+else {
+    echo "you cannot drive";
+}
+
+echo "done";
+
+
+
+
+?>
