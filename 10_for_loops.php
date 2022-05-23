e470e0d345ecd31f19a8dd9b900c4ed7c37fe07b
Abhishek Dhar <68233166+Codeblocc@users.noreply.github.com>
2022-05-23 20:55:58 +0530

    Add files via upload

---
+++ 10_for_loops.php
@@ -0,0 +1,14 @@
+<?php
+
+// The for loop - Loops through a block of code a specified number of times.
+
+echo "this is for loop in action <br>";
+
+for ($index=0; $index < 5; $index++) {
+    echo "the number is $index <br>";
+}
+
+// Avoid running into infinite loops
+
+
+?>
