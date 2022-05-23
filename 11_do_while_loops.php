6feda1c61bbed2cbaf7e46a09a3927a11c840289
Abhishek Dhar <68233166+Codeblocc@users.noreply.github.com>
2022-05-23 21:20:01 +0530

    Add files via upload

---
+++ 11_do_while_loops.php
@@ -0,0 +1,12 @@
+<?php
+
+// The do...while loop - Loops through a block of code once, and then repeats the loop as long as the specified condition is true.
+
+$i = 0;
+do{
+    echo "$i <br>";
+    $i++;
+}while($i<5);
+
+
+?>
