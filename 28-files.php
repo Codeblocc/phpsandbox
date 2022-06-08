319a1e4ffd6770f9abaecf200b83b0b8c8eb18d9
Abhishek Dhar <68233166+Codeblocc@users.noreply.github.com>
2022-06-08 23:42:50 +0530

    Update 28-files.php

--- 28-files.php
+++ 28-files.php
@@ -1,6 +1,12 @@
 <?php
 
-$a = readfile("testfile.txt");
-echo $a;
+// $a = readfile("testfile.txt");
+// echo $a;
+
+// the browser will print "test file test file 12324". The last characters are the number of characters. 24 is the number of characters.
+
+// readfile("testfile.txt");
+// this will print without the character count.
+readfile("testfile.html");
 
 ?>
